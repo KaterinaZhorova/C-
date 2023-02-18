4455d0fac9b0e7a75979a6d414d9f79db8b45411
Katerina Zhorova <zhorova85@mail.ru>
2023-02-18 13:30:08 +0300

    первая задача семинар 1 с#

---
+++ HelloConsole/Домашнее задание семинар 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите число namber1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("введите число namber2: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2);
+{
+    Console.WriteLine ($"максимальное число ({number1}) минимальное число ({number2})");
+}
+if(number2 > number1);
+{ 
+    Console.WriteLine($"минимальное число({number2}) максимальное число ({number1})");    
+}
